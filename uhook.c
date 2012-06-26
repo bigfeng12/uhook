0956ded4dd7610909d546bba084027da52fb3a18
bigfeng12 <peiyong.feng.kernel@gmail.com>
2012-06-26 19:51:55 +0800

    Add copyright decleration
    Signed-off-by: bigfeng12 <peiyong.feng.kernel@gmail.com>

--- uhook.c
+++ uhook.c
@@ -1,3 +1,18 @@
+/*
+ *      uhook (call kernel function from userspace)driver for Linux.
+ *
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ *	peiyong.feng<peiyong.feng.kernel@gmail.com>
+ */
 #include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/fs.h>
